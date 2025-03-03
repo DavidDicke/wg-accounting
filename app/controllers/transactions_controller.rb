5cdcdebdb8153f83f7ae4de7a74991aedf54dbe4
David Dicke <post@david-dicke.de>
2025-03-03 14:14:30 +0100

    Add dotenv-rails

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,24 @@
+class TransactionsController < ApplicationController
+  def index
+    @transactions = Transaction.all
+  end
+
+  def new
+    @transaction = Transaction.new
+  end
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+    if @transaction.save
+      redirect_to transactions_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def transaction_params
+    require(:transaction).permit(:amount, :date, :transaction_type, :bank_account_id)
+  end
+end
